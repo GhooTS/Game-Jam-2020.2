de00eee91ee9b71bf73aab501aa63843f2479fd0
krzysztof biernat <krzysztof.biernat@edu.uekat.pl>
2020-08-02 17:14:15 +0200

    Add character controller and setup character camera

---
+++ Assets/MoveController.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class MoveController : MonoBehaviour
+{
+    public CharacterController2D controller;
+    public float moveSpeed = 10f;
+    private float moveDirection;
+    private bool jump;
+
+
+    private void Update()
+    {
+        moveDirection = Input.GetAxis("Horizontal");
+        jump = Input.GetKeyDown(KeyCode.Space);
+    }
+
+    private void FixedUpdate()
+    {
+        controller.Move(moveDirection * moveSpeed * Time.fixedDeltaTime,false,jump);
+    }
+}
