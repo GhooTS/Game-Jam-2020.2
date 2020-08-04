280396586b2f21be970666e2a6dcd29615d4fc22
krzysztof biernat <krzysztof.biernat@edu.uekat.pl>
2020-08-04 19:15:38 +0200

    Add inteaction system

---
+++ Assets/Scripts/Interaction/Interactable.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+public class Interactable : MonoBehaviour,IInteractable
+{
+    public UnityEvent onInteraction;
+
+    public void Interact()
+    {
+        onInteraction?.Invoke();    
+    }
+}
---
+++ Assets/Scripts/Interaction/PlayerInteraction.cs
@@ -0,0 +1,72 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.InputSystem;
+
+public class PlayerInteraction : MonoBehaviour
+{
+    public string interactionObjectName;
+    public LayerMask interactionMask;
+    [Min(0.1f)]
+    public float interactionRadius;
+    public IInteractable interactable;
+    [Header("Events")]
+    public UnityEvent onInteracted;
+    public UnityEvent onInteractionChanged;
+    public UnityEvent onInteractionLost;
+
+    public PlayerInteraction(float interactionRadius)
+    {
+        this.interactionRadius = interactionRadius;
+    }
+
+    private void FixedUpdate()
+    {
+        var result = Physics2D.CircleCastAll(transform.position, interactionRadius,Vector2.zero,Mathf.Infinity,interactionMask);
+
+        if (result.Length == 0)
+        {
+            if(interactable != null)
+            {
+                interactable = null;
+                interactionObjectName = "";
+                onInteractionLost?.Invoke();
+            }
+            return;
+        }
+
+        var oldInteractable = interactable;
+        var closes = Mathf.Infinity;
+        for (int i = 0; i < result.Length; i++)
+        {
+            if (result[i].collider.TryGetComponent(out IInteractable interactable))
+            {
+                var distance = Vector2.Distance(result[i].point, result[i].transform.position);
+                if(closes > distance)
+                {
+                    this.interactable = interactable;
+                    closes = distance;
+                    interactionObjectName = result[i].transform.name;
+                }
+            }
+        }
+
+        if(oldInteractable != interactable)
+        {
+            onInteractionChanged?.Invoke();
+        }
+    }
+
+    public void Interact(InputAction.CallbackContext ctx)
+    {
+        if(ctx.canceled && ctx.phase == InputActionPhase.Canceled && interactable != null)
+        {
+            interactable.Interact();
+            onInteracted?.Invoke();
+        }
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.DrawWireSphere(transform.position, interactionRadius);
+    }
+}
