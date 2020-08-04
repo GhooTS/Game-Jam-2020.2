517d40c326285064378945bcb0c1c3d09871e698
krzysztof biernat <krzysztof.biernat@edu.uekat.pl>
2020-08-04 19:12:36 +0200

    Add Damage system

---
+++ Assets/Scripts/Health/DealDamageTrigger.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class DealDamageTrigger : MonoBehaviour
+{
+    public string colliderTag;
+    public float damage;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.CompareTag(colliderTag) && collision.TryGetComponent(out IDamageable damageable))
+        {
+            damageable.TakeDamage(damage);
+        }
+    }
+}
---
+++ Assets/Scripts/Health/Health.cs
@@ -0,0 +1,62 @@
+﻿using GTVariable;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Health : MonoBehaviour, IDamageable
+{
+    public FloatReference HP;
+    public FloatReference MaxHP;
+    [Header("Events")]
+    public UnityEvent OnDamaged;
+    public UnityEvent OnDeath;
+
+    public bool Alive { get; private set; } = false;
+
+    private void OnEnable()
+    {
+        Restard();
+    }
+
+    public void Restard()
+    {
+        HP.Value = MaxHP.Value;
+        Alive = true;
+    }
+
+    public void Restard(float HP)
+    {
+        this.HP.Value = Mathf.Min(HP,MaxHP);
+        Alive = true;
+    }
+
+    public void TakeDamage(float damage)
+    {
+        if(Alive)
+        {
+            HP.Value -= damage;
+            OnDamaged?.Invoke();
+            TryKill();
+        }
+    }
+
+    private void TryKill()
+    {
+        if (HP.Value <= 0f) 
+        {
+            Kill();
+        }
+    }
+
+    public void Kill()
+    {
+        if (Alive == false) return;
+
+        if(HP.Value > 0)
+        {
+            HP.Value -= HP.Value;
+            OnDamaged?.Invoke();
+        }
+        Alive = false;
+        OnDeath?.Invoke();
+    }
+}
