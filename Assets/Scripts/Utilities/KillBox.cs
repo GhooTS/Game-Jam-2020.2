5c67b309829b6e764636b124af7d516e558f57c0
krzysztof biernat <krzysztof.biernat@edu.uekat.pl>
2020-08-05 17:17:16 +0200

    Add kill box class

---
+++ Assets/Scripts/Utilities/KillBox.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class KillBox : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            collision.GetComponent<Health>().Kill();
+        }
+    }
+}
