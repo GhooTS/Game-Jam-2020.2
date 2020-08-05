8e1889e185732fdf45af7e528eb4f1e4d663b342
krzysztof biernat <krzysztof.biernat@edu.uekat.pl>
2020-08-05 17:14:34 +0200

    Add character audio controller and disable unity sound system

---
+++ Assets/Scripts/Controllers/CharacterAudioController.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class CharacterAudioController : MonoBehaviour
+{
+
+    public void PlaySound(string path)
+    {
+        FMODUnity.RuntimeManager.PlayOneShot(path, transform.position);
+        Debug.Log($"Play sound {path}");
+    }
+
+    public void PlayFootstepSound(string path)
+    {
+        PlaySound(path);
+    }
+
+    public void PlayAttackSound(string path)
+    {
+        PlaySound(path);
+    }
+}
