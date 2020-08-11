21a53e396c435fb127af761b372d49630e8ea1e1
krzysztof biernat <krzysztof.biernat@edu.uekat.pl>
2020-08-11 16:11:12 +0200

    Add particle emitter and global particle

---
+++ Assets/Scripts/Utilities/GlobalParticle.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(menuName = "Spawners/Particle Spawner")]
+public class GlobalParticle : ScriptableObject
+{
+    public ParticleSystem prefab;
+    private ParticleSystem instance;
+
+    private void OnDisable()
+    {
+        if(instance != null)
+        {
+            Destroy(instance);
+        }
+    }
+
+    public void Spawn(Vector3 position,int amount)
+    {
+        if(instance == null)
+        {
+            instance = Instantiate(prefab);
+        }
+
+        instance.transform.position = position;
+        instance.Emit(amount);
+    }
+}
---
+++ Assets/Scripts/Utilities/ParticleEmitter.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class ParticleEmitter : MonoBehaviour
+{
+    public GlobalParticle particle;
+
+
+    public void Emit(int amount)
+    {
+        particle.Spawn(transform.position, amount);
+    }
+}
